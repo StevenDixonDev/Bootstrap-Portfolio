5bc2c7daabfb712893d43a7d3fcf74209a0c73b9
StevenDixonDev <stevedixon1128.dev@gmail.com>
2019-07-03 15:56:15 -0400

    updated portfolio with bootstrap and jquery goodness

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,4 +1,66 @@
 // Use jquery to handle click events on portfolio items
-$(".card").click(function(){
-  window.open($(this).attr("data"));
+// $(".card").click(function(){
+//   window.open($(this).attr("data"));
+// })
+//./assets/images/portfolio
+
+const allowedProjects = [
+  {name: "Word-Guess-Game", picture: "Guess the Word.png"},
+  {name: "unit-4-game", picture: "Starwars.jpg"},
+]
+
+
+
+$(document).ready(function(){
+  $.ajax({
+    url: 'https://api.github.com/users/stevenDixonDev',
+    method: 'GET'
+  }).then(function(res){
+    console.log(res);
+    $(".user-image").attr("src", res.avatar_url);
+    $(".user-about").append(`<h4>${res.bio}</h4>`);
+    $(".user-info").append(`<h4>${res.name}</h4>`);
+    $(".user-info").append(`<h4>@${res.login}</h4>`);
+    $(".user-contact").append(`<h6><i class="fas fa-link"></i> <a href="${res.html_url}">${res.html_url}</a></h6>`);
+    $(".user-contact").append(`<h6><i class="fas fa-map-marker-alt"></i> ${res.location}</h6>`);
+  })
+
+  $.ajax({
+    url: 'https://api.github.com/users/stevenDixonDev/repos',
+    method: 'GET'
+  }).then(function(res){
+    console.log(res)
+    $.each(res, function(index, project){
+      console.log(project)
+      $.each(allowedProjects, function(index, allowed){
+        if(allowed.name === project.name){
+          $("#projects").append(`
+          <div class="col-md-12 col-lg-6 mb-3">
+          <div class="card bg-dark text-white">
+          <img class="card-img-top" src="./assets/images/portfolio/${allowed.picture}" alt="${project.name}" />
+          <div class="card-body">
+            <h4 class="card-title">${project.name}</h4>
+            <h5 class="card-text">${project.description}</h5>
+            <div class="d-flex flex-row">
+                <p><i class="fas fa-code"></i> ${project.language}</p>
+                <p class="ml-3"><i class="fas fa-star"></i> ${project.stargazers_count}</p>
+                <p class="ml-3"><i class="fas fa-code-branch"></i> ${project.forks} </p>
+            </div>
+          </div>
+          </div>
+          </div>
+          `);
+        }
+      })
+      //
+    })
+  })
 })
+
+{/* <img class="user-image" src=""/>
+            <div class="user-info">
+name
+            </div>
+            <div class="user-contact">
+
+</div> */}
